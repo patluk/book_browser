98bde31e0ec0ec5782067cfe011c758b36c7c31e
patluk <48183644+patluk@users.noreply.github.com>
2022-01-17 02:52:35 +0100

    Update settings.py

--- book_browser/settings.py
+++ book_browser/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -133,3 +133,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATICFILES_DIRS = (
+    ('assets', 'book_browser/book_browser/wsgi/openshift/static'),
+
+    )
